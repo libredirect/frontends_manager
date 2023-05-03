b287b6596304906429900f5514dcac329941dd6b
ManeraKai <manerakai@protonmail.com>
2023-05-03 22:28:13 +0300

    Added Docker require message. Getting ready for beta release

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ function loadJSON(file) {
 let frontends_running = {};
 (async () => {
     const config = await loadJSON("config.json")
+    let isDockerInstalled = await ipcRenderer.invoke('isDockerInstalled')
     for (const key in config) {
         if (
             !(process.platform == "linux" && config[key].command_linux)
@@ -30,6 +31,15 @@ let frontends_running = {};
                 <img src="assets/imgs/${config[key].icon}">
                 <h1>${config[key].name}</h1>
             </a>
+            `;
+        if (config[key].docker && !isDockerInstalled) {
+            tile.innerHTML += 'Requires <a class="underline" href="https://www.docker.com" target="_blank">Docker</a>'
+            document.getElementById("frontends").appendChild(tile)
+            document.getElementById(`${key}-link`).style.pointerEvents = 'none'
+            document.getElementById(`${key}-link`).style.userSelect = 'none'
+            document.getElementById(`${key}-link`).style.opacity = .4
+        } else {
+            tile.innerHTML += `
             <button id="${key}-log">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"
                     fill="currentColor">
@@ -39,58 +49,74 @@ let frontends_running = {};
                 Log
             </button>
             <button id="${key}-run">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"
-                    fill="currentColor">
-                    <path d="M320 856V296l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z" />
-                </svg>
-                Run
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"
+                fill="currentColor">
+                <path d="M320 856V296l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z" />
+            </svg>
+            <span>Run</span>
             </button>
             <button id="${key}-close">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"
                     fill="currentColor">
                     <path d="M320 416v320-320Zm-80 400V336h480v480H240Zm80-80h320V416H320v320Z" />
                 </svg>
-                Close
+                <span>Close</span>
             </button>
-            `;
-
-        document.getElementById("frontends").appendChild(tile)
-
-        document.getElementById(`${key}-log`).addEventListener("click", () => {
-            ipcRenderer.send('log', key)
-        })
-
-        function running_buttons(name) {
-            if (frontends_running[name]) {
-                document.getElementById(`${name}-link`).style.pointerEvents = ''
-                document.getElementById(`${name}-link`).style.userSelect = ''
-                document.getElementById(`${name}-link`).style.opacity = 1
-                document.getElementById(`${name}-run`).style.display = 'none'
-                document.getElementById(`${name}-close`).style.display = ''
-                document.getElementById(`${name}-log`).style.display = ''
-            } else {
-                document.getElementById(`${name}-link`).style.pointerEvents = 'none'
-                document.getElementById(`${name}-link`).style.userSelect = 'none'
-                document.getElementById(`${name}-link`).style.opacity = .4
-                document.getElementById(`${name}-run`).style.display = ''
-                document.getElementById(`${name}-close`).style.display = 'none'
-                document.getElementById(`${name}-log`).style.display = 'none'
-            }
-        }
-        frontends_running[key] = false
-        document.getElementById(`${key}-run`).addEventListener("click", () => {
-            ipcRenderer.invoke('run', key)
-                .then((msg) => {
+            `
+            document.getElementById("frontends").appendChild(tile)
+            document.getElementById(`${key}-run`).addEventListener("click", () => {
+                ipcRenderer.invoke('run', key)
+                    .then((msg) => {
+                        frontends_running[key] = msg;
+                        running_buttons(key)
+                    })
+                document.getElementById(`${key}-run`).getElementsByTagName('span')[0].innerHTML = "Be patient it's not broken..."
+                document.getElementById(`${key}-run`).style.pointerEvents = 'none'
+                document.getElementById(`${key}-run`).style.userSelect = 'none'
+                document.getElementById(`${key}-run`).style.opacity = .4
+            })
+            document.getElementById(`${key}-close`).addEventListener("click", () => {
+                ipcRenderer.invoke('close', key).then((msg) => {
                     frontends_running[key] = msg;
                     running_buttons(key)
                 })
-        })
-        document.getElementById(`${key}-close`).addEventListener("click", () => {
-            ipcRenderer.invoke('close', key).then((msg) => {
-                frontends_running[key] = msg;
-                running_buttons(key)
+                document.getElementById(`${key}-close`).getElementsByTagName('span')[0].innerHTML = "Closing..."
+                document.getElementById(`${key}-close`).style.pointerEvents = 'none'
+                document.getElementById(`${key}-close`).style.userSelect = 'none'
+                document.getElementById(`${key}-close`).style.opacity = .4
             })
-        })
-        running_buttons(key)
+            document.getElementById(`${key}-log`).addEventListener("click", () => {
+                ipcRenderer.send('log', key)
+            })
+
+            frontends_running[key] = false
+            function running_buttons(name) {
+                if (frontends_running[name]) {
+                    document.getElementById(`${name}-link`).style.pointerEvents = ''
+                    document.getElementById(`${name}-link`).style.userSelect = ''
+                    document.getElementById(`${name}-link`).style.opacity = 1
+                    document.getElementById(`${name}-run`).style.display = 'none'
+                    document.getElementById(`${name}-run`).style.display = 'none'
+                    document.getElementById(`${key}-run`).getElementsByTagName('span')[0].innerHTML = "Run"
+                    document.getElementById(`${name}-run`).style.pointerEvents = ''
+                    document.getElementById(`${name}-run`).style.userSelect = ''
+                    document.getElementById(`${name}-run`).style.opacity = 1
+                    document.getElementById(`${name}-close`).style.display = ''
+                    document.getElementById(`${name}-log`).style.display = ''
+                } else {
+                    document.getElementById(`${name}-link`).style.pointerEvents = 'none'
+                    document.getElementById(`${name}-link`).style.userSelect = 'none'
+                    document.getElementById(`${name}-link`).style.opacity = .4
+                    document.getElementById(`${name}-run`).style.display = ''
+                    document.getElementById(`${key}-close`).getElementsByTagName('span')[0].innerHTML = "Close"
+                    document.getElementById(`${name}-close`).style.pointerEvents = ''
+                    document.getElementById(`${name}-close`).style.userSelect = ''
+                    document.getElementById(`${name}-close`).style.opacity = 1
+                    document.getElementById(`${name}-close`).style.display = 'none'
+                    document.getElementById(`${name}-log`).style.display = 'none'
+                }
+            }
+            running_buttons(key)
+        }
     }
 })()
--- src/main.js
+++ src/main.js
@@ -1,92 +1,123 @@
 const { app, shell, BrowserWindow, Menu, Tray, ipcMain } = require('electron')
-const dockerCompose = require('docker-compose');
+const { v2 } = require('docker-compose');
+const dockerCompose = v2;
 const fs = require('fs');
 
 let showWin = true
 let frontendsProcesses = {}
 let frontendsProcessesDocker = []
 app.whenReady().then(() => {
-    const win = new BrowserWindow({
-        icon: `${__dirname}/assets/imgs/icon.png`,
-        width: 1200,
-        height: 800,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    })
-    win.loadFile(`${__dirname}/index.html`)
+    (async () => {
+        try {
+            if ((await dockerCompose.version()).err === '') isDockerInstalled = true
+        } catch (_) { }
 
-    win.webContents.setWindowOpenHandler(({ url }) => {
-        if (url.startsWith('http')) {
-            shell.openExternal(url)
-            return { action: 'deny' }
-        }
-        return { action: 'allow' }
-    })
-    // win.removeMenu()
-    win.maximize()
-
-    ipcMain.on('log', (_, name) => {
-        let path
-        if (process.platform == "linux") {
-            path = `${__dirname}/log/${name}.log`
-        } else if (process.platform == "win32") {
-            path = `${__dirname}\\log\\${name}.log`
-        }
-        if (!fs.existsSync(path)) return
-        const frontendWin = new BrowserWindow({
-            title: config[name].name,
-            icon: `${__dirname}/assets/imgs/${config[name].icon}`,
-            width: 900,
-            height: 800,
-        })
-        frontendWin.loadFile(path)
-        let watcher = fs.watch(path, function (event, _) {
-            if (event == 'change') {
-                frontendWin.reload()
+        for (const key in config) {
+            const val = config[key]
+            if (!val.docker) {
+                if (process.platform == "linux" && val.command_linux) {
+                    run_frontends[key] = () => run_frontend(key, val.command_linux, val.args, val.env)
+                } else if (process.platform == "win32" && val.command_windows) {
+                    run_frontends[key] = () => run_frontend(key, val.command_windows, val.args, val.env)
+                }
+            } else if (isDockerInstalled) {
+                run_frontends[key] = () => run_frontend_docker(key)
             }
-        });
-        frontendWin.on("close", () => watcher.close())
-    })
-
-    ipcMain.handle('run', async (_, name) => {
-        await run_frontends[name]();
-        return true
-    })
-
-    ipcMain.handle('close', async (_, name) => {
-        if (name in frontendsProcesses) {
-            frontendsProcesses[name].kill('SIGINT');
         }
-        else if (frontendsProcessesDocker.includes(name)) {
-            const r = await dockerCompose.stop({ cwd: `${__dirname}/frontends/${name}`, log: true });
+        if (process.platform == "linux") {
+            run_frontend('caddy', './caddy_linux_amd64', ['run'])
+            run_frontend('redis', './redis-stable/src/redis-server', ['./redis.conf'])
+            while (!(await cehck_port(6379))) { }
+        } else if (process.platform == "win32") {
+            run_frontend('caddy', '.\\caddy_windows_amd64.exe', ['run'])
         }
-        return false
-    })
 
-    const trayIcon = new Tray(`${__dirname}/assets/imgs/icon.png`)
-    const contextMenu = Menu.buildFromTemplate([
-        { label: 'Show', type: 'normal', click: () => { showWin = true; win.show(); } },
-        { label: 'Hide', type: 'normal', click: () => { showWin = false; win.hide(); } },
-        { type: 'separator' },
-        { label: 'Quit', type: 'normal', click: () => { showWin = false; app.quit(); } }
-    ])
+        const win = new BrowserWindow({
+            icon: `${__dirname}/assets/imgs/icon.png`,
+            width: 1200,
+            height: 800,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false,
+            },
+        })
+        win.loadFile(`${__dirname}/index.html`)
 
-    win.addListener("close", e => {
-        if (showWin) {
-            showWin = false
-            win.hide()
-            e.preventDefault()
-        }
-    })
+        win.webContents.setWindowOpenHandler(({ url }) => {
+            if (url.startsWith('http')) {
+                shell.openExternal(url)
+                return { action: 'deny' }
+            }
+            return { action: 'allow' }
+        })
+        // win.removeMenu()
+        win.maximize()
 
-    trayIcon.addListener("click", () => {
-        showWin = !showWin
-        if (!showWin) win.hide()
-        else win.show()
-    })
-    trayIcon.setContextMenu(contextMenu)
+        ipcMain.on('log', (_, name) => {
+            let path
+            if (process.platform == "linux") {
+                path = `${__dirname}/log/${name}.log`
+            } else if (process.platform == "win32") {
+                path = `${__dirname}\\log\\${name}.log`
+            }
+            if (!fs.existsSync(path)) return
+            const frontendWin = new BrowserWindow({
+                title: config[name].name,
+                icon: `${__dirname}/assets/imgs/${config[name].icon}`,
+                width: 900,
+                height: 800,
+            })
+            frontendWin.loadFile(path)
+            let watcher = fs.watch(path, function (event, _) {
+                if (event == 'change') {
+                    frontendWin.reload()
+                }
+            });
+            frontendWin.on("close", () => watcher.close())
+        })
+
+        ipcMain.handle('run', async (_, name) => {
+            await run_frontends[name]();
+            return true
+        })
+
+        ipcMain.handle('close', async (_, name) => {
+            if (name in frontendsProcesses) {
+                frontendsProcesses[name].kill('SIGINT');
+            }
+            else if (frontendsProcessesDocker.includes(name)) {
+                const r = await dockerCompose.stop({ cwd: `${__dirname}/frontends/${name}`, log: true });
+            }
+            return false
+        })
+
+        ipcMain.handle('isDockerInstalled', async () => {
+            return isDockerInstalled;
+        })
+
+        const trayIcon = new Tray(`${__dirname}/assets/imgs/icon.png`)
+        const contextMenu = Menu.buildFromTemplate([
+            { label: 'Show', type: 'normal', click: () => { showWin = true; win.show(); } },
+            { label: 'Hide', type: 'normal', click: () => { showWin = false; win.hide(); } },
+            { type: 'separator' },
+            { label: 'Quit', type: 'normal', click: () => { showWin = false; app.quit(); } }
+        ])
+
+        win.addListener("close", e => {
+            if (showWin) {
+                showWin = false
+                win.hide()
+                e.preventDefault()
+            }
+        })
+
+        trayIcon.addListener("click", () => {
+            showWin = !showWin
+            if (!showWin) win.hide()
+            else win.show()
+        })
+        trayIcon.setContextMenu(contextMenu)
+    })();
 })
 
 function getEnv(env) {
@@ -110,15 +141,15 @@ function run_frontend(name, command, args, env) {
         let child
         if (env) {
             if (process.platform == "linux") {
-                child = spawn(command, args, { cwd: __dirname + `/frontends/${name}`, env: getEnv(env) })
+                child = spawn(command, args, { cwd: `${__dirname}/frontends/${name}`, env: getEnv(env) })
             } else if (process.platform == "win32") {
-                child = spawn(command, args, { cwd: __dirname + `\\frontends\\${name}`, env: getEnv(env) })
+                child = spawn(command, args, { cwd: `${__dirname}\\frontends\\${name}`, env: getEnv(env) })
             }
         } else {
             if (process.platform == "linux") {
-                child = spawn(command, args, { cwd: __dirname + `/frontends/${name}` })
+                child = spawn(command, args, { cwd: `${__dirname}/frontends/${name}` })
             } else if (process.platform == "win32") {
-                child = spawn(command, args, { cwd: __dirname + `\\frontends\\${name}` })
+                child = spawn(command, args, { cwd: `{__dirname}\\frontends\\${name}` })
             }
         }
         frontendsProcesses[name] = child
@@ -154,6 +185,7 @@ function run_frontend_docker(name) {
                     fs.appendFileSync(path, result.err);
                 },
                 err => {
+                    resolve(false)
                     console.log('something went wrong:', err.message)
                 }
             );
@@ -176,32 +208,9 @@ function cehck_port(port) {
 }
 
 let run_frontends = {}
-const config = JSON.parse(fs.readFileSync(__dirname + '/config.json'))
-for (const key in config) {
-    const val = config[key]
-    if (!val.docker) {
-        if (process.platform == "linux" && val.command_linux) {
-            run_frontends[key] = () => run_frontend(key, val.command_linux, val.args, val.env)
-        } else if (process.platform == "win32" && val.command_windows) {
-            run_frontends[key] = () => run_frontend(key, val.command_windows, val.args, val.env)
-        }
-    } else {
-        run_frontends[key] = () => run_frontend_docker(key)
-    }
-}
+const config = JSON.parse(fs.readFileSync(`${__dirname}/config.json`))
+let isDockerInstalled = false;
 
-(async () => {
-    if (process.platform == "linux") {
-        run_frontend('caddy', './caddy_linux_amd64', ['run'])
-        run_frontend('redis', './redis-stable/src/redis-server', ['./redis.conf'])
-        while (!(await cehck_port(6379))) { }
-    } else if (process.platform == "win32") {
-        run_frontend('caddy', '.\\caddy_windows_amd64.exe', ['run'])
-    }
-    // for (const k of Object.keys(run_frontends)) {
-    //     run_frontends[k]()
-    // }
-})();
 
 let readyToQuite = false
 app.on("before-quit", async event => {
